b1714813ffa23d545e5f3594ebcbfc9654ad4ff2
AleksK <a.karagodin.001@gmail.com>
2026-05-17 20:58:13 +0300

    HW_6

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,9 +1,24 @@
-import pytest
-from selenium import webdriver
+# from pages.swag_labs import SwagLabs
+#
+#
+# def test_check_swag(browser):
+#     swag_page = SwagLabs(browser)
+#     swag_page.visit()
+#     assert swag_page.exist_icon()
+
+# from pages.swag_labs import SwagLabs
+#
+#
+# def test_check_username_field(browser):
+#     swag_page = SwagLabs(browser)
+#     swag_page.visit()
+#     assert swag_page.find_element(locator="#user-name")
 
 
-@pytest.fixture(scope='session')
-def browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+from pages.swag_labs import SwagLabs
+
+
+def test_check_password_field(browser):
+    swag_page = SwagLabs(browser)
+    swag_page.visit()
+    assert swag_page.find_element(locator="#password")
