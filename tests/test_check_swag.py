66dfcb794d9e89fab3aeb9b5b6f6d055f1521912
AleksK <a.karagodin.001@gmail.com>
2026-05-21 11:59:40 +0300

    HW_6

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,21 +1,17 @@
-# from pages.swag_labs import SwagLabs
-#
-#
-# def test_check_swag(browser):
-#     swag_page = SwagLabs(browser)
-#     swag_page.visit()
-#     assert swag_page.exist_icon()
-
-# from pages.swag_labs import SwagLabs
-#
-#
-# def test_check_username_field(browser):
-#     swag_page = SwagLabs(browser)
-#     swag_page.visit()
-#     assert swag_page.find_element(locator="#user-name")
-
-
 from pages.swag_labs import SwagLabs
+
+
+def test_check_swag(browser):
+    swag_page = SwagLabs(browser)
+    swag_page.visit()
+    assert swag_page.exist_icon()
+
+
+def test_check_username_field(browser):
+    swag_page = SwagLabs(browser)
+    swag_page.visit()
+    assert swag_page.find_element(locator="#user-name")
+
 
 
 def test_check_password_field(browser):
